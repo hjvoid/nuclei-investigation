60862ffa74fb60d4e6ec0686f2445b6dddc1bf2f
Hugo Jeffreys <hugo.jeffreys@digital.cabinet-office.gov.uk>
2024-01-16 12:07:52 +0000

    Remove unused packages and files

--- index.ts
+++
@@ -1,9 +0,0 @@
-import figlet from "figlet";
-
-const server = Bun.serve({
-  fetch() {
-    const body = figlet.textSync("Nuclei \nInvestigation", {font: "Swamp Land"});
-    return new Response(body);
-  },
-  port: 3000,
-});
